9b43fcc70b354b38db4e714cebb87866a17e42ef
Krys <jackpschmid@gmail.com>
2016-12-07 09:07:10 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Kata{
+    public static void main(String[] args){
+        String binary = "";
+        int n = 1234;
+        int bits = 0;
+            while (n > 0){
+            long remainder = n % 2;
+            binary += remainder;
+            n /= 2;
+        }
+        for (int i = 0; i < binary.length(); i++){
+            if (binary.substring(i, i + 1).equals("1")){
+                bits++;
+            }else{}
+        }
+    }
+}

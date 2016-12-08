f3084657c84a535e5404ccb928fb998758b13ea1
Krys <jackpschmid@gmail.com>
2016-12-07 17:40:00 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
-public class Kata{
-    public static void main(String[] args){
-        String binary = "";
-        int n = 1234;
-        int bits = 0;
-            while (n > 0){
-            long remainder = n % 2;
-            binary += remainder;
-            n /= 2;
-        }
-        for (int i = 0; i < binary.length(); i++){
-            if (binary.substring(i, i + 1).equals("1")){
-                bits++;
-            }else{}
-        }
+public class BitCounting {
+
+	public static int countBits(int n){
+	  String binary = "";
+    int n = 1234;
+    int bits = 0;
+    while (n > 0){
+      long remainder = n % 2;
+      binary += remainder;
+      n /= 2;
     }
+    for (int i = 0; i < binary.length(); i++){
+      if (binary.substring(i, i + 1).equals("1")){
+        bits++;
+      }else{}
+	}
 }
